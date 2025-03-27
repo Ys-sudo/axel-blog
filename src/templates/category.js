5fd4569d2231dbc3f0b93033c09503721e5b8b22
Ys-sudo <georgios.lazaridis0@gmail.com>
2025-03-27 05:34:44 +0100

    cats for build

--- src/templates/category.js
+++ src/templates/category.js
@@ -9,14 +9,18 @@ import FullWidthImage from "../components/FullWidthImage";
 import HeroImg from "../img/axel-travel-kategoria-podroze.jpg";
 const CategoryTemplate = ({ pageContext }) => {
   const { category, categoryPosts } = pageContext;
+
   let CoolImg = { isCat: true, url: null };
   CoolImg.url = HeroImg;
+
+  const capitalizeFirstLetter = (str) =>
+    str.charAt(0).toUpperCase() + str.slice(1);
   return (
     <Layout>
       <FullWidthImage
         img={CoolImg}
-        title={"Artykuły z kategori"}
-        subheading={category}
+        title={capitalizeFirstLetter(category)}
+        subheading={`Artykuły z kategori ${category}`}
       />
       <div className="bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100">
         <section className="py-16 px-6 md:px-12">
@@ -45,40 +49,8 @@ const CategoryTemplate = ({ pageContext }) => {
                             : ""
                         } bg-white dark:bg-gray-900 transition-all rounded-xl`}
                       >
-                        <header>
-                          {post?.frontmatter?.featuredimage && (
-                            <div className="relative w-full h-48 mb-6 overflow-hidden rounded-lg shadow-md">
-                              <PreviewCompatibleImage
-                                imageInfo={{
-                                  image: post.frontmatter.featuredimage,
-                                  alt: `featured image thumbnail for post ${post.frontmatter.title}`,
-                                  width:
-                                    post.frontmatter.featuredimage
-                                      .childImageSharp.gatsbyImageData.width,
-                                  height:
-                                    post.frontmatter.featuredimage
-                                      .childImageSharp.gatsbyImageData.height,
-                                }}
-                              />
-                            </div>
-                          )}
-                          <p className="text-gray-800 dark:text-white">
-                            <Link
-                              className="text-2xl font-bold text-primary dark:text-primary-light hover:text-primary-dark transition-all"
-                              to={post.fields.slug}
-                            >
-                              {post.frontmatter.title}
-                            </Link>
-                            <span className="mx-2">•</span>
-                            <span className="text-sm text-gray-600 dark:text-gray-400">
-                              {post.frontmatter.date}
-                            </span>
-                          </p>
-                        </header>
+                        {post.id}
                         <p className="text-gray-700 dark:text-gray-300 mt-4 text-base leading-relaxed">
-                          {post.excerpt}
-                          <br />
-                          <br />
                           <Link
                             className="inline-block text-primary dark:text-primary-light hover:text-primary-dark font-medium transition-all"
                             to={post.fields.slug}
